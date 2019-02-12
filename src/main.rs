bc3976e8a3974d0ef098caca0e4fad1cd07747a6
Alec Goncharow <Alec.Goncharow@gmail.com>
2019-02-12 18:08:22 -0500

    add uncc campus edge list and route to access

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,30 @@ extern crate router;
 use std::env;
 use iron::{status, Iron, IronResult, Request, Response};
 use router::Router;
+use std::fs::File;
+use std::io::prelude::*;
+
 
 // Serves a string to the user.  Try accessing "/".
 fn hello(_: &mut Request) -> IronResult<Response> {
     let resp = Response::with((status::Ok, "Hello world!"));
+
+    Ok(resp)
+}
+
+// simple route to allow for quick prototyping
+fn campus_response(_: &mut Request) -> IronResult<Response> {
+    let mut file = match File::open("static/campus.json") {
+        Ok(f) => f,
+        Err(e) => panic!("{:?}", e)
+    };
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+    let mut resp = Response::new();
+    resp.body = Some(std::boxed::Box::new(contents));
+    resp.status = Some(status::Ok);
+    resp.headers.set(iron::headers::ContentType::json());
+
     Ok(resp)
 }
 
@@ -22,6 +42,8 @@ fn get_server_port() -> u16 {
 fn main() {
     let mut router: Router = Router::new();
     router.get("/", hello, "index");
+    router.get("/uncc_campus", campus_response, "uncc_campus");
+
 
     Iron::new(router)
         .http(("0.0.0.0", get_server_port()))
