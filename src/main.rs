efde0b290bc9ac090eb4f2649d86e58d96a16ad1
Alec Goncharow <Alec.Goncharow@gmail.com>
2019-02-12 17:36:44 -0500

    initial iron setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate iron;
+extern crate router;
+
+use std::env;
+use iron::{status, Iron, IronResult, Request, Response};
+use router::Router;
+
+// Serves a string to the user.  Try accessing "/".
+fn hello(_: &mut Request) -> IronResult<Response> {
+    let resp = Response::with((status::Ok, "Hello world!"));
+    Ok(resp)
+}
+
+/// Look up our server port number in PORT, for compatibility with Heroku.
+fn get_server_port() -> u16 {
+    env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(8080)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut router: Router = Router::new();
+    router.get("/", hello, "index");
+
+    Iron::new(router)
+        .http(("0.0.0.0", get_server_port()))
+        .unwrap();
 }
